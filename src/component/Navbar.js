b071b51c60950076b0d750c9dbab3f0bf2b8d1d7
Sahan Hansaja <165319843+sahanHansaja026@users.noreply.github.com>
2024-12-24 00:40:49 +0530

    update navbar 0.2.0

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -6,10 +6,16 @@ export default class Navbar extends Component {
     return (
       <div className="navbar">
         <div className="navtitle">
-          <div className="cicon">C</div>
-          <div className="h1class">
-            <h1 className="h1class1">Today</h1>
-            <h1 className="h1class2">Thursday , 14 November | 6 Tasks</h1>
+          <div className="dateandicon">
+            <div className="cicon">C</div>
+            <div className="h1class">
+              <h1 className="h1class1">Today</h1>
+              <h1 className="h1class2">Thursday , 14 November | 6 Tasks</h1>
+            </div>
+          </div>
+
+          <div className="rightside">
+            <button className='addtask'>Add Task +</button>
           </div>
         </div>
       </div>
