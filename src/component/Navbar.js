0c2baae0212ea80cadecfad602020754ad5c2845
Sahan Hansaja <165319843+sahanHansaja026@users.noreply.github.com>
2024-12-24 00:26:24 +0530

    navibar modify 0.1.0

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,7 +4,15 @@ import "../css/navbar.css";
 export default class Navbar extends Component {
   render() {
     return (
-      <div className='navbar'>Navbar</div>
-    )
+      <div className="navbar">
+        <div className="navtitle">
+          <div className="cicon">C</div>
+          <div className="h1class">
+            <h1 className="h1class1">Today</h1>
+            <h1 className="h1class2">Thursday , 14 November | 6 Tasks</h1>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
