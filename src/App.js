095891ef36e6cd2bb479a9bdf816030da670b1c6
Sahan Hansaja <165319843+sahanHansaja026@users.noreply.github.com>
2024-12-24 00:05:44 +0530

    user interface modify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
-function App() {
+import Landingpage from "./component/landing_page";
+import Navbar from "./component/Navbar";
+
+const App = () => {
+  const location = useLocation();
+
+  const noNavBarPaths = ["/login", "/registar"];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {!noNavBarPaths.includes(location.pathname) && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Landingpage />} />
+      </Routes>
     </div>
   );
-}
+};
 
-export default App;
+export default () => (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
---
+++ src/component/Navbar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import "../css/navbar.css";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div className='navbar'>Navbar</div>
+    )
+  }
+}
